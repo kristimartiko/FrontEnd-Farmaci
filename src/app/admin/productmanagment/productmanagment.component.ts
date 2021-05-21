64660a8b1a974f8a18be2c0ad0c2d9605aff7eed
Kristi <kristi.martiko@fshnstudent.info>
2021-05-21 17:26:21 +0200

    product managment created

---
+++ src/app/admin/productmanagment/productmanagment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productmanagment',
+  templateUrl: './productmanagment.component.html',
+  styleUrls: ['./productmanagment.component.css']
+})
+export class ProductmanagmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
