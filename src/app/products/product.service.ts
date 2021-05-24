f05c6195fdc86b0c75088aedb1f9226b6fe43577
Kristi <kristi.martiko@fshnstudent.info>
2021-05-24 19:34:36 +0200

    product service finish

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -13,5 +13,16 @@ export class ProductService {
     return this.http.get<Product[]>('http://127.0.0.1:8000/api/getAllProducts');
   }
 
+  addNewProduct(formValue: any) {
+    return this.http.post<Product>('http://127.0.0.1:8000/api/addProduct', formValue);
+  }
+
+  deleteProduct(p: Product) {
+    return this.http.delete(`http://127.0.0.1:8000/api/delete/${p.product_id}`);
+  }
+
+  updateProduct(p: Product) {
+    return this.http.put<Product>(`http://127.0.0.1:8000/api/delete/${p.product_id}`, p);
+  }
   
 }
